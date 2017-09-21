a346b28a0281d20e21adc26aea29780364f54385
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-09-20 21:25:51 -0700

    form credentials set

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -23,6 +24,29 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Global Vars
+app.use(function(req, res, next) {
+  res.locals.errors = null;
+  next(); 
+});
+
+//Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+  while(namespace.length) {
+    formParam += '[' + namespace.shift() + ']';
+  }
+  return {
+    param: formParam,
+    msg: msg,
+    value: value
+    };
+  }
+}));
 
 var users = [
   {
@@ -53,7 +77,27 @@ app.get('/', function(req, res) {
 });
 
 app.post('/users/add', function(req, res) {
-  console.log(req.body.first_name);
+
+  req.checkBody('first_name', 'First Name is Required').notEmpty(); 
+  req.checkBody('last_name', 'Last Name is Required').notEmpty(); 
+  req.checkBody('email', 'Email is Required').notEmpty(); 
+
+  var errors = req.validationErrors();
+
+    if(errors) {
+      res.render('index', {
+        title: 'Customers',
+        users: users,
+        errors: errors
+        });
+    } else {
+      var newUser = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email
+      }
+      console.log('SUCCESS')
+    }    
 });
 
 
